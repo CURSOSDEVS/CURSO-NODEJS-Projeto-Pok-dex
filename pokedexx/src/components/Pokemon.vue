2baf02ffaf52fc4a47cf7b1cb3395a721b57f84c
Claudisnei Alves <claudisneibello@gmail.com>
2021-12-26 13:02:14 -0300

    Melhorando o aspecto do componente, utilizando css da biblioteca bulma

--- pokedexx/src/components/Pokemon.vue
+++ pokedexx/src/components/Pokemon.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        <h1> {{num}} - {{name | upper}}</h1>
-        <small>{{url}}</small>
+    <div id="pokemon">
+        <div class="card">
+            <div class="card-image">
+                <figure>
+                <img :src="pokemon.front" alt="Placeholder image">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">{{num}} - {{name | upper}}</p>
+                        <p class="subtitle is-6">{{pokemon.type}}</p>
+                    </div>
+                </div>
+
+                <div class="content">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,18 +27,21 @@ import axios from 'axios';
 export default {
     created:function(){
         axios.get(this.url).then(res=>{
-            //console.log(res);
+            console.log(res);
             this.pokemon.type = res.data.types[0].type.name;
             this.pokemon.front = res.data.sprites.front_default;
             this.pokemon.back = res.data.sprites.back_default;
             console.log(this.pokemon);
         })
-
     },
 
     data(){
         return{
-            pokemon: {}
+            pokemon: {
+                type: ' ',
+                front: ' ',
+                back: ' '
+            }
         }
     },
 
@@ -38,12 +57,11 @@ export default {
           return newName;
       }
   }
-
-
- 
 }
 </script>
 
 <style>
-
+    #pokemon{
+        margin-top: 1%;
+    }
 </style>
